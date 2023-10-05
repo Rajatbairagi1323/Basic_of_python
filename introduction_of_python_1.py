be2e144ecbd87c7362ec09adfd2f7d69214eded5
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-10-05 21:32:27 +0530

    Add files via upload

---
+++ introduction_of_python_1.py
@@ -0,0 +1,90 @@
+# Language Fundamentals
+
+# --Introduction--
+
+# python is a general purpose high level programming language.
+# python was developed by Guido Van Rossam in 1998 while working at National Research institude at Neatherland.
+# But offially python was made available  to public in 1991. The official date of birth for python is : Feb 20th 1991.
+# python is recommended as first programming language for  beginner.
+
+
+
+# To print HelloWorld :-
+
+# Java
+
+# 1) public class HelloWorld
+# 2)     {
+# 3)     p s v main (string[]args)
+# 4)      {
+# 5)       SOP('HelloWorld');
+# 6)      }
+# 7)        }
+
+
+
+# C
+
+# 1) # include<studio.h>
+# 2) void main()
+# 3) {
+# 4)   print("HelloWorld");
+# 5) }
+
+
+
+# python
+
+
+print("HelloWorld")
+
+
+
+# python sum of two numbers
+
+
+a=10
+b=20
+print("The sum":,(a+b))
+
+
+
+# The name python was selected from the TV show -- THE COMPLETE MONTY PYTHON'S CIRCUS -- which was broadcaster in BBC from 1969 to 1974.
+
+
+# Guido developed python language by taking almost all programming features from different languages.
+
+# 1-- Functional programming features from C.
+# 2-- object oriented programming features from C++.
+# 3-- Modular programming features from modula -3.
+  
+  
+# Most of syntax in python Derived from C and ABC languages.
+
+
+
+
+
+# Where we can use python.
+
+# We can use everywhere. The most comman important application areas are :-
+
+# 1) for developing desktop applications.
+# 2) for developing web applications.
+# 3) for developing database applications.
+# 4) for networking porgramming.
+# 5) for developing games.
+# 6) for data analysis applications.
+# 7) for machine learning.
+# 8) for developing artificial intelligence applications.
+# 9) for IOT.
+
+
+
+# --Note--
+
+# Internally google and youtube use python coding.
+# NASA and Nework stock exchange applications develop by python.
+# Top software companies like google, microsoft, IBM, yahoo using python.
+
+
