b6a34bba81cf300d62c001f09bd1993b6ea218e9
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-10-05 21:33:31 +0530

    Add files via upload

---
+++ identifiers_in_python_3.py
@@ -0,0 +1,71 @@
+# ----- IDENTIFIERS -----
+
+# A name in python program is called identifiers.
+# It can be class name or function name or module name or variable name.
+
+# a = 10
+
+
+# --RULES TO DEFINE IDENTIFIERS IN PYTHON--
+
+# 1) The only only allowed character in python are :--
+
+# * Alphabate symbols (either lower case or upper case)
+# * digits(0 to 9) 
+# * underscore symbol(_).
+
+# By mistake if we are using any other symbol like $ then we will get syntax error.
+
+# * cash=10 correct
+# * ca$h=20 wrong because python are not allow to use dollor symbol
+
+
+
+# 2) Identifiers should not start with digit.
+
+# 123total wrong
+# total123 correct
+
+
+# 3) Identifiers are case sensitive of course python language is case sensitive language.
+
+total = 10
+TOTAL = 1000
+print(total) #----> 10
+print(TOTAL) #----> 1000
+
+
+
+# ---IDENTIFIERS---
+
+# 1) Alphabet symbol (Either upper case or lower case )
+# 2) If identifiers is started underscore (_) then it indicates it is private.
+# 3) Identifiers should not start with Digits.
+# 4) Identifiers are case sensitive.
+# 5) we cannot used reserved words as indentifiers.  EX. def=10 wrong
+# 6) There is no length limit for python identifiers. But not recommended use too lengthy identifiers.
+# 7) Dollor ($) symbol is not allowed in python.
+
+
+
+# Which of the following are vaild python identifiers?
+
+# 123total -->  wrong
+# total123  --> right
+# java2share --> right 
+# ca$h --> wrong
+# _abc_abc_ --> right
+# def --> wrong because it is keyword
+# if --> wrong because it is keywords
+
+
+
+# ---NOTE---
+
+
+# 1) If identifiers start with _ symbol then it indicates that is private.
+# 2) If identifiers start with __ two underscore symbol it indicating that strongly private identifiers.
+# 3) If the identifiers starts and ends that identifiers is language defined special name which is also known as (MAGIC METHODS).
+# EX. --> __add__   
+
+
