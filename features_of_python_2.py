33a105ab9ce175bac47618a98f21bec8923dc25b
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-10-05 21:32:57 +0530

    Add files via upload

---
+++ features_of_python_2.py
@@ -0,0 +1,153 @@
+# ---Features of python---
+
+
+# 1) SIMPLE AND EAST TO LEARN--
+
+
+# python is a simple programming language when we read python program, use can feel like reading english statements.
+# the syntax are very simple and noly 30+ keywords are available.
+# when compared with other language we can write program with very less number of lines. Hence more readible and simplicity.
+# we can reduce development and coast of the project.
+
+
+# 2) FREEWARE AND OPEN SOURCE--
+
+# we can use python without licence and it is freeware.
+# its source code is open, so that we can costomize based on our requirement.
+# EX- jython is customized virson of python to work with java applications.
+
+
+
+# 3) HIGH LEVEL PROGRAMMING LANGUAGE--
+
+# python is high level programming language and hence it is programmer friendly language being programmer we are not required to concentrate 
+# low level activities like memory management and security etc.
+
+
+
+# 4) PLATFORM INDEPENDENT--
+
+# once we write python program, it can run on any platform without rewriting once again internally. PVM is responsible to convert into 
+# machine understable form.
+
+
+
+# 5) PORTABILITY--
+
+# python programs are portable .ie we can migrate from one platform to another platform very easily. python program will provide same result on any 
+# platform.
+
+
+
+# 6) DYNAMICALLY TYPED--
+
+# In python we are not required to declare type for variable. whenever we are assinging the value, based on value, type will be allocated
+# automatically. Hence python is considered as dynamically typed language.
+# But java, C etc are statically typed language because we have to provide type at beginning only.
+# this dynamic typing nature will provide more flexibility to the programmer.
+
+
+
+# 7) BOTH PROCEDURE ORIENTED AND OBJECT ORIENTED--
+
+# python language support both procedure oriented like (C, pascal) etc.. and object oriented like (C++, java) features, Hence we can get benefits
+# of both like security and reusability etc.
+
+
+
+# 8) INTERPRETED--
+	
+# we are not required to compile python programs explicitly. Internally python interpreter will take care that complilation.
+# Its compailation fails interpreter raised syntax error. once compilation success the PVM (python virtual machine) is responsible to execute.
+
+
+
+# 9) Extensible--
+
+# we can use other language programs in python are:
+# * we can use already existing legacy non - python code.
+# * we can improve performance of the application.
+
+
+
+# 10) EMBEDDED--
+
+# we can use python programs in any other language programs. i.e.. we can embedded python programs anywhere.
+
+
+
+
+# 11) EXTENSIVE LIBRABY--
+
+# python has a rich inbuit library.
+# Being a programmer we can use this library directly and we are not responsible to implement the funcationality etc..
+
+
+
+
+# ------LIMITATIONS OF PYTHON------
+
+# performance wise not up to mark because it is interpreter language.
+# not using for mobile applications.
+
+
+
+
+# --- FLAVOURS OF PYTHON ---
+
+
+# 1) CPython--
+
+# It is the standard flavours of python. It can be used to work with C language applications.
+
+
+# 2) Jython/JPython--
+
+# It is for java application. It can be run on JVM (java virtual machine).
+
+
+# 3) IronPython--
+
+# It is for C #. Net platform.
+
+
+# 4) PyPy--
+
+# The main advantage of PyPy is performance will be improved because JIT (just-in-time) compiler is availabe inside PVM.
+
+
+# 5) RubyPython--
+
+# for Ruby platform.
+
+
+# 6) AnacondaPython--
+
+# It is specially designed for handling large volume of data processing.
+
+
+
+
+
+
+# ----- PYTHON VERSION -----
+
+
+# python 1.0 version introduced in jan 1994.
+
+# python 2.0 version introduced in october 2000.
+
+# python 3.0 version introduced in december 2008.
+
+
+
+# NOTE:--
+
+
+# python 3 won't provide backward compatibility to python 2. i.e.. there is no gurantee that python2 program will be run in python3.
+
+
+# current version 
+
+# python --> 3.6.1       python --> 3.7.0 
+
