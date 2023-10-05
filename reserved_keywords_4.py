1fd7b8eeddb8c0ebcea0b84864b5e7bbd7098e42
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-10-05 23:38:29 +0530

    reserved keywords

---
+++ reserved_keywords_4.py
@@ -0,0 +1,41 @@
+# ------RESERVED KEYWORDS -------
+
+# In python some words are reserved to represent some meaning or funcationality. such type of words are called reserved words.
+
+
+# There are 33 reserved words available in python
+
+# True, Flase, None
+# and, or, not, is
+# if, else, elif
+# while, for, break, continue, return, in, yield
+# try, except, finally, raise, assert
+# import, from, as, class, def, pass, global, nonlocal, lambda, del, with
+
+
+
+# NOTE---
+
+# All reserved words in python contains only alphabet symbols.
+# Except the following 3 reserved words, all contain, only lower case alphabet symbols.
+
+
+# True
+# Flase
+# None
+
+# Ex.... a = true --> wrong
+# Ex.... a = True --> right
+
+
+
+import keyword
+keyword.kwlist
+
+
+
+
+
+# ['False','None','True','and','as','assert','break','class','continue','def','del','elif','else','except','finally','for','from','global','if'
+#  'import','in','is','lambda','nonlocal','not','or','pass','raise','return','try','while','with','yield']
+
